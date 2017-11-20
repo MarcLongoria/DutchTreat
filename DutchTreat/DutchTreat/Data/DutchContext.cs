bcc4154d86dcda4543dffc8dd309f36a5926872b
Marc Longoria <marclongoria@outlook.com>
2017-11-20 16:59:01 -0600

    Entities added along with DB Context and configuration of SQL DB

---
+++ DutchTreat/DutchTreat/Data/DutchContext.cs
@@ -0,0 +1,22 @@
+﻿using DutchTreat.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DutchTreat.Data
+{
+    public class DutchContext : DbContext 
+    {
+        public DutchContext(DbContextOptions<DutchContext> options) : base(options)
+        {
+
+        }
+
+
+        DbSet<Product> Product { get; set; }
+        DbSet<Order> Orders { get; set; }
+    }
+}
